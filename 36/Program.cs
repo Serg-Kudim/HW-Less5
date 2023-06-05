d33598d4636e109a0b3f8466838208301bbfb139
Serg-Kudim <Serg.Kudim@yandex.ru>
2023-06-05 22:11:04 +0300

    Решение 36

---
+++ 36/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях.
+
+
+int InputNumber()
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void CreateArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+int SumNumNegativIndex(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i % 2 != 0){
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+Console.Write("Enter the size of the array: ");
+int size = InputNumber();
+int[] array = new int[size];
+CreateArray(array);
+PrintArray(array);
+Console.WriteLine(SumNumNegativIndex(array));
